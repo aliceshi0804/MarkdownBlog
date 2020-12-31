013cc3bf5e182e3850d6604152ac82ce0b5704da
Qing Shi <aliceshi0804@gmail.com>
2020-12-30 16:45:33 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article')
+const articelRouter = require('./routes/articles');
+const methodOverride = require('method-override')
+const app = express();
+
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+app.set("view engine", "ejs");
+// can access our article form inside our article route
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: 'desc' })
+    res.render('articles/index', { articles: articles })
+});
+
+app.use("/articles", articelRouter);
+
+app.listen(5000);
